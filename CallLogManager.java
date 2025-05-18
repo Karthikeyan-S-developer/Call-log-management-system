4e6990cb5b94f7a6ab6421b11183fb85fd6ae59c
Karthikeyan S <karthikeya4255@gmail.com>
2025-05-18 08:04:37 +0530

    Update CallLogManager.java

--- CallLogManager.java
+++ CallLogManager.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class CallLogManager {
     private static final String URL = "jdbc:mysql://localhost:3306/CallLogDB";
     private static final String USER = "root";
-    private static final String PASSWORD = "SKy@12345";
+    private static final String PASSWORD = "password";//Replace with your password
 
     public static void main(String[] args) {
         try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
